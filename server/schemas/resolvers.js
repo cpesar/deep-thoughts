7b085ba52e8a7675f877e7300e95424e79611ec1
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-04 11:12:54 -0600

    settup server queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,31 @@
+// const Thought = require("../models/Thought");
+const { User, Thought } = require('../models');
 
 
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello World'
+     // Get all Users
+     users: async() => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+
+    // Get a user by username
+    user: async (parent, {username}) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({  _id });
     }
   }
 };
