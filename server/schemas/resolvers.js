2636b9f049926ef18878a66620da2911f1dc274b
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-04 14:33:39 -0600

    unable to login using token

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,33 @@
 // const Thought = require("../models/Thought");
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if(context.user){
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+
      // Get all Users
-     users: async() => {
+    users: async() => {
       return User.find()
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
     },
+
+   
 
     // Get a user by username
     user: async (parent, {username}) => {
@@ -18,14 +35,40 @@ const resolvers = {
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
-    },
+      },
+
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
+      },
+
+      thought: async (parent, { _id }) => {
+        return Thought.findOne({  _id });
+      }
     },
 
-    thought: async (parent, { _id }) => {
-      return Thought.findOne({  _id });
+  Mutation: {
+    addUser: async(parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return  { token, user };
+    },
+
+    login: async(parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if(!user) {
+        throw new AuthenticationError('Incorrect Credentials');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+
+      if(!correctPw){
+        throw new AuthenticationError('Incorrect Credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     }
   }
 };
--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 // Import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,7 +13,8 @@ const app = express();
 // Create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // Integrate our Apollo server with the Express application as middleware
