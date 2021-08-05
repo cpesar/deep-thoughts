d3cab7a527721e86eea81593b9f8f6730e3a1ecc
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-05 13:10:09 -0600

    set up server and client to work with react app

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // Import Apollo Server
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 // Import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
@@ -22,6 +23,14 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
