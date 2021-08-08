ad03f034f7847c22dadbdf563d146acc3e98dea2
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-08 13:56:16 -0600

    add a friend, add a reaction

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,11 +1,39 @@
 
 import React, { useState } from 'react';
 // import React from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
 
   const [thoughtText, setText] = useState(' ');
   const [characterCount, setCharacterCount] = useState(0);
+
+  // useMutation hook 
+  const [addThought, {error}] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought }}) {
+      // Wrap it in a try/catch in case the thought doesn't exist yet
+      try{
+        // read what's currently in the cache
+         const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+         // prepend the newest thought tot he front of the array
+      cache.writeQuery({
+        query: QUERY_THOUGHTS,
+        data: { thoughts: [addThought,...thoughts]}
+      });
+    } catch (e) {
+      console.error(e);
+    }
+
+    // Update me object's cache, appending new thought to the end of the array
+    const { me } = cache.readQuery({ query: QUERY_ME });
+    cache.writeQuery({
+      query: QUERY_ME,
+      data: { me: { ...me, thoughts: [...me.thoughts, addThought]}}
+    });
+  }
+});
 
   const handleChange = event => {
     if(event.target.value.length <= 280){
@@ -16,14 +44,28 @@ const ThoughtForm = () => {
 
   const handleFormSubmit = async event => {
     event.preventDefault();
-    setText('');
-    setCharacterCount(0);
+    
+    try{
+      // add thought to database
+      await addThought({
+        variables: { thoughtText }
+      });
+
+      // clear from value
+      setText('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
   };
+
+
 
   return (
     <div>
-      <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
         Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
       <form 
         className="flex-row justify-center justify-space-between-md align-stretch"
